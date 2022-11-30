6df1073855a878fde4641e70e6e4670cc1060f91
주영 <jungjuyoung1314@gmail.com>
2022-11-30 13:26:50 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/HelloSpirngApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpirngApplication.class, args);
+	}
+
+}
